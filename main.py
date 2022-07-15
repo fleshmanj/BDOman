35d51f301f1445bfc021114a929210255383eae9
fleshmanj <fleshmanjoshuac@hotmail.com>
2022-07-15 16:44:47 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+Write only one statement per line.
+Write what you mean, not how to program it
+Give proper indentation to show hierarchy and make code understandable.
+Make the program as simple as possible.
+Conditions and loops must be specified well ie. begun and ended explicity as in given pseudocode examples :
+"""
+
+# make a varible to hold screenshot
+
+# crop screenshot to only view that is needed
+
+#
